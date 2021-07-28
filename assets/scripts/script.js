8dc361a1a1cf61ebede1a59301f7b860ff0ae2f2
Sven Lowry <sven.lowry@gmail.com>
2021-07-28 13:57:25 +0100

    Add function to add all game elements to HTML

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,2 +1,23 @@
 let gameElements = ['rock','paper','scissors'];
 
+addElementButtons();
+/*
+Icons:
+<i class="fas fa-hand-rock"></i>
+<i class="fas fa-hand-scissors"></i>
+<i class="fas fa-hand-paper"></i>
+*/
+
+function addElementButtons() {
+    let buttonArea = document.getElementById("game-buttons")
+
+    let html = "";
+    for(let gameElement of gameElements) {
+        html += `
+            <div class="game-button">
+                <button id="${gameElement}" type="button"><i class="fas fa-hand-${gameElement}"></i></button> 
+            </div>
+        `
+    }
+    buttonArea.innerHTML = html;
+}
